1ec61b2207440e07df67831b5eaacc1af379a772
KajalDeore04 <kajaldeore04@gmail.com>
2024-12-05 09:44:30 +0530

    minor

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -88,7 +88,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Roshni Kumari</Logo>
+        <Logo>Kajal Deore</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
